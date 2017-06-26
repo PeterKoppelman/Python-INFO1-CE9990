762bfd84a1d82fe17282acdd7006eb4ac748d563
PeterKoppelman <PeterKoppelman@users.noreply.github.com>
2017-06-25 22:15:43 -0400

    Create homework week 2.py

---
+++ homework week 2.py
@@ -0,0 +1,65 @@
+
+"""
+graphpaper.py
+
+this program creates a graph. Input items are rows, columns and the spaces between the rows and columns
+"""
+
+import sys
+
+def getInt(prompt):
+
+    """
+    Let the user input an integer
+    """
+
+    assert isinstance(prompt, str)
+
+    while True:
+
+        try:
+            s = input(prompt)
+        except EOFError:
+            sys.exit(1)
+        
+        # if user has pressed the enter key exit the program
+        if not s:
+            print("Thank you... exiting routine")
+            sys.exit(0)
+            
+        try:
+            i = int(s)
+        except ValueError:
+            print("I'm sorry ", s, " is not an integer")
+            continue
+        return i
+
+
+while True:
+  no_rows = getInt("How many rows of boxes: ")
+  no_columns = getInt("How many columns of boxes: ")
+  spaces_rows = getInt("How many rows of spaces are in each box: ")
+  spaces_col = getInt("How many columns spaces are in each box: ")
+    
+  i_counter = 0
+  # loop while counter < number of rows that we want. Print the horizontal line every time i_counter increments
+  # put the str("+") on the end to finish off the right hand side of the box
+  while i_counter < no_rows:
+    horizontal_lines = no_columns * (str("+") + (spaces_col * str("-"))) + str("+")
+    print(horizontal_lines)
+    
+    vert = 0
+    # calculate the number of vertical rows that we need. Print them out every time vert increments.
+    # put the str("|") on the end to finish off the right hand side of the box
+
+    vertical_lines = no_columns * (str("|") + (spaces_col * str(" "))) + str("|")
+    while vert < spaces_rows:
+        print(vertical_lines)
+        vert+= 1
+
+    i_counter += 1
+
+  # print out the horizontal line again to finish off the bottom row.  
+  print(horizontal_lines)
+
+  sys.exit(0)
