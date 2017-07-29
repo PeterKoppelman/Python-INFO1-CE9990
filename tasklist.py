79450303936161450cac310b4b0b7369596f4066
PeterKoppelman <PeterKoppelman@users.noreply.github.com>
2017-07-29 16:25:41 -0400

    Update tasklist.py

--- tasklist.py
+++ tasklist.py
@@ -8,17 +8,28 @@ import sys
 import os
 import csv
 
-#Create a child process and a pipe as a csv file
-infile = os.popen("tasklist /FO csv")   
-lines = infile.readlines()      #lines is a list of lines.
+# tasklist ists all of the processes that are running.
+# The format (/fo csv) outputs everything as a csv file.
+# /nh strips out the header. We can use all lines of output.
+infile = os.popen("tasklist /fo csv /nh")   
 
-lines = [line.split(",") for line in lines]
+# windows does not give you a nmessage that tells you that the
+# popen command was successful or not (i.e. the way that Linux or Mac
+# gives you a status result of None). As long as you spell everything
+# correctly it works.
+
+lines = csv.reader(infile)
+
+# lines = [line.split(",") for line in lines]
 lines = sorted(set([line[0] for line in lines]))
 
 print("           Image Name")
 print("    =========================")
       
 for i, line in enumerate(lines, start = 1):
-    print("{:3} {}".format(i, line[1:-1])) # [1:-1] strips out the quotes
+#     # print("{:3} {}".format(i, line[1:-1])) # [1:-1] strips out the quotes
+    print("{:3} {}".format(i, line)) 
 
+infile.close()
 sys.exit(0)
+
