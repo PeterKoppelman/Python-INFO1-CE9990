c5234e818206bc8732df1665a5442740d784d999
PeterKoppelman <PeterKoppelman@users.noreply.github.com>
2017-07-29 23:14:44 -0400

    Update tasklist.py

--- tasklist.py
+++ tasklist.py
@@ -8,28 +8,24 @@ import sys
 import os
 import csv
 
-# tasklist ists all of the processes that are running.
-# The format (/fo csv) outputs everything as a csv file.
-# /nh strips out the header. We can use all lines of output.
+# tasklist lists all of the processes that are running.
+# The /fo csv outputs everything as a csv file.
+# /nh strips out the header.
 infile = os.popen("tasklist /fo csv /nh")   
-
-# windows does not give you a nmessage that tells you that the
-# popen command was successful or not (i.e. the way that Linux or Mac
-# gives you a status result of None). As long as you spell everything
-# correctly it works.
-
 lines = csv.reader(infile)
-
-# lines = [line.split(",") for line in lines]
 lines = sorted(set([line[0] for line in lines]))
+
+status = infile.close()
+# Check status of operation. Success is None. failure is anything but None. 
+if status != None:      
+    # print("\"tasklist statement\" produced exit status", status)
+    print("tasklist statement produced exit status", status)
+    sys.exit(1)
 
 print("           Image Name")
 print("    =========================")
       
 for i, line in enumerate(lines, start = 1):
-#     # print("{:3} {}".format(i, line[1:-1])) # [1:-1] strips out the quotes
     print("{:3} {}".format(i, line)) 
 
-infile.close()
 sys.exit(0)
-
